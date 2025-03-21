76cf22871bacdcbd87915059649c28da9a3b299a
mcmikem <1mark2mike@gmail.com>
2025-03-21 12:18:03 +0200

    Update App.js
    Fix unused variables in App.js

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 
 function App() {
   return (
